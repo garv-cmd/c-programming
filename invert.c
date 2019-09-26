373a2ffff19a993188e02694abeda5dfc75988f0
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2019-09-26 11:51:43 +0530

    Update invert.c

--- invert.c
+++ invert.c
@@ -3,16 +3,12 @@
 
 int main()
 {
-   /*int num,n,res;
+   int num,n,res;
    scanf("%d",&num);
    scanf("%d",&n);
    res=num ^ (1<<n);
-   printf("%d",res);*/
-   int num;
-   scanf("%d",&num);
-   if (num&1==1)
-    printf("set");
-   else printf("unset");
+   printf("%d",res);
+   
 
     return 0;
 }
